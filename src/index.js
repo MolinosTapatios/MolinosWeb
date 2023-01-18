71f229518b2f3cc8a260f12e6b227c071e2c2a4f
Silvino Aguiar <2121100431@soy.utj.edu.mx>
2023-01-18 11:27:55 -0600

    post

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './css/bootstrap.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <h1>React....</h1>
   <React.StrictMode>
     <App />
   </React.StrictMode>
